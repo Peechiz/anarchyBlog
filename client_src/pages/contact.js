31d617c8146e5a0e9d150c6cc4b44c2904aa5c8b
Chris Impicciche <cimpicci@gmail.com>
2018-06-27 18:45:52 -0500

    adds googlemap

--- client_src/pages/contact.js
+++ client_src/pages/contact.js
@@ -1,9 +1,20 @@
 import Layout from '../components/layout'
+import MapContainer from '../components/googlemap'
+import '../css/contact.css'
 
-const Contact = () => (
-  <Layout>
-    <div>Contact</div>
-  </Layout>
-)
+const Contact = () => {
+  return (
+    <Layout>
+      <main className="contact">
+        <div id="map">
+          <MapContainer/>
+        </div>
+        <div className="content">
+          <h1>Contact</h1>
+        </div>
+      </main>
+    </Layout>
+  )
+}
 
 export default Contact
