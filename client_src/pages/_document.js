e063dfd85949fa185906ddcb997c431e62157ed5
Chris Impicciche <cimpicci@gmail.com>
2018-06-26 19:23:41 -0500

    adds magic meta tag

--- client_src/pages/_document.js
+++ client_src/pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>Austin Anarchy</title>
+          <meta name="viewport" content="width=device-width"/>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"/>
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
