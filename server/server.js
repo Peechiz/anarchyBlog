4774f8cfb281caa64dd16a5dc27df25f10b53dc7
Chris Impicciche <cimpicci@gmail.com>
2018-08-15 12:20:18 -0500

    initial oauth skeleton

--- server/server.js
+++ server/server.js
@@ -1,29 +1,36 @@
-'use strict';
-
 var loopback = require('loopback');
-var boot = require('loopback-boot');
-
+var path = require('path');
 var app = module.exports = loopback();
+// Create an instance of PassportConfigurator with the app instance
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+app.boot(__dirname);
+...
+// Enable http session
+app.use(loopback.session({ secret: 'keyboard cat' })); // TODO: change secret
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+// Load the provider configurations
+var config = {};
+try {
+  config = require('./providers.json');
+} catch (err) {
+  console.error('Please configure your passport strategy in `providers.json`.');
+  console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+  process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+// Set up related models
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
 });
+// Configure passport strategies for third party auth providers
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
