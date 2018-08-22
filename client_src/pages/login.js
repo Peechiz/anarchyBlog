b13e1916ac1330ca0386bd2970b99bd6d854c832
Chris Impicciche <cimpicci@gmail.com>
2018-08-21 20:25:08 -0500

    edits login

--- client_src/pages/login.js
+++ client_src/pages/login.js
@@ -1,29 +1,12 @@
 import { Component } from 'react'
 import '../css/login.css'
 
+const BASE_URL = process.env.BASE_URL || ''
+
 class Login extends Component {
   constructor(){
     super();
-    this.state = {
-      'email':'',
-      'password':''
-    }
-    this.updateUser = this.updateUser.bind(this)
-    this.updatePassword = this.updatePassword.bind(this)
-    this.submit = this.submit.bind(this)
-  }
 
-  updateUser(e){
-    this.setState({ email: e.target.value })
-  }
-  
-  updatePassword(e) {
-    this.setState({ password: e.target.value })
-  }
-
-  submit(e){
-    e.preventDefault()
-    console.log(this.state)
   }
 
   render(){
@@ -31,17 +14,10 @@ class Login extends Component {
       <div className="login">
         <form>
           <img src="static/img/logo.png"/>
-          <h1>Login</h1>
-          <input 
-            type="text" 
-            onChange={this.updateUser}
-            placeholder="Email"/>
 
-          <input type="password" 
-            onChange={this.updatePassword}
-            placeholder="Password"/>
-
-          <button onClick={this.submit}>Submit</button>
+          <a href={`${BASE_URL}auth/google`}>
+            <button>Login with the Googs</button>
+          </a>
         </form>
       </div>
     )
