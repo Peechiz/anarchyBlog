551a92a0f59f8d110125748b77670ca31ead9328
Chris Impicciche <cimpicci@gmail.com>
2018-08-21 21:15:36 -0500

    adds admin page

---
+++ client_src/pages/admin.js
@@ -0,0 +1,10 @@
+const Admin = () => {
+    return (
+        <div>
+            <h1>Secret Admin Page</h1>
+            <p>In theory you can manange who can edit or author a blog post here</p>
+        </div>
+    )
+}
+
+export default Admin;
