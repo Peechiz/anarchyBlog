189b99a09683bc1b81e04e0143f056aeb3b48979
Chris Impicciche <cimpicci@gmail.com>
2018-10-07 19:03:25 -0500

    updates contact

--- pages/contact.js
+++ pages/contact.js
@@ -11,6 +11,14 @@ const Contact = () => {
         </div>
         <div className="content">
           <h1>Contact</h1>
+          <div className="contact-info">
+            <div>
+              <p>Amet commodo elit magna esse ullamco exercitation consequat reprehenderit elit in anim fugiat. Commodo id ea nisi proident. Non esse ipsum quis proident. Excepteur culpa ullamco labore et voluptate commodo est mollit. Culpa culpa do veniam aliquip et ullamco ullamco do est nostrud consequat velit exercitation fugiat. Est nisi est dolor id quis ex eiusmod quis dolore sit eiusmod laborum nostrud. Occaecat elit velit sit laboris veniam pariatur nostrud et dolor do voluptate eu.</p>
+            </div>
+            <div>
+              <h3>info@austinAnarchy.com</h3>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
