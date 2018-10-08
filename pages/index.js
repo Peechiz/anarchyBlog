547d6a517a05955c3aafc70b91a46836bb278562
Chris Impicciche <cimpicci@gmail.com>
2018-10-07 19:04:20 -0500

    moves initial props to parent component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
+import { Component } from 'react'
 import Layout from '../components/layout'
 import Blogroll from '../components/blogroll'
+import { loadDB } from '../lib/db'
 
-const Index = () => (
-  <Layout>
-    <Blogroll/>
-  </Layout>
-)
+
+class Index extends Component {
+
+  static async getInitialProps(){
+    let db = await loadDB();
+    let result = await new Promise( (resolve, reject) => {
+      db.collection('posts')
+        .orderBy('date', 'desc')
+        .limit(10)
+        .get()
+        .then(snapshot => {
+          let arr = []
+          snapshot.forEach(post => {
+            arr.push({
+              id: post.id,
+              ...post.data()
+            })
+          })
+          resolve(arr)
+        }).catch(err => {
+          console.log(err)
+          reject([])
+      })
+    })
+    return {posts: result};
+  }
+
+  render(){
+    return (
+      <Layout>
+        <Blogroll posts={this.props.posts}/>
+      </Layout>
+    )
+  }
+}
 
 export default Index
