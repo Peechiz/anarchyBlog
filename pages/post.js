0e3036a03e11703e09491ac5ebc7e60b004e74f0
Chris Impicciche <cimpicci@gmail.com>
2018-10-07 19:02:14 -0500

    adds firebase

--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,9 @@
 import Layout from '../components/layout'
-import fetch from 'isomorphic-unfetch'
+import NextPost from '../components/nextPost'
+import { loadDB } from '../lib/db'
 import ReactMarkdown from 'react-markdown'
 import Moment from 'moment'
 import '../css/post.css'
-
-const BASE_URL = process.env.BASE_URL || ''
 
 const Post = (props) => {
   const date = Moment().calendar(props.date)
@@ -15,7 +14,15 @@ const Post = (props) => {
           <div className="content-wrapper post">
             <h1>{props.title}</h1>
             <h2>{date}</h2>
+            {/* <h2>id: {props.id}</h2> */} 
+
             <ReactMarkdown source={props.text}/>
+
+            <NextPost type="previous" id={props.prev}></NextPost>
+            <NextPost type="next" id={props.next}></NextPost>
+
+            {/* { props.next ? <a href={`/post?id=${props.next}`}>Next</a> : null}
+            { props.prev ? <a href={`/post?id=${props.prev}`}>Previous</a> : null} */}
           </div>
         </div>
         <div className="hero"></div>
@@ -25,9 +32,48 @@ const Post = (props) => {
 }
 
 Post.getInitialProps = async ({query}) => {
-  const res = await fetch(`${BASE_URL}api/posts?filter[where][id]=${query.id}`)
-  const json = await res.json()
-  return json[0] || {}
+  let db = await loadDB();
+  let post = await db.collection('posts').doc(query.id).get().then(post => {
+    return {
+      id: post.id,
+      ...post.data()
+    };
+  })
+
+  let next = await db.collection('posts')
+    .where('date', '<', post.date)
+    .orderBy('date', 'desc')
+    .limit(1)
+    .get()
+    .then(next => {
+      try {
+        return next.docs[0].id
+      } catch (err) {
+        return null
+      }
+  })
+
+  let prev = await db.collection('posts')
+    .where('date', '>', post.date)
+    .orderBy('date', 'asc')
+    .limit(1)
+    .get()
+    .then(prev => {
+      try {
+        return prev.docs[0].id
+      } catch (err) {
+        return null
+      }
+  })
+
+  return {
+    next,
+    prev,
+    title: post.title,
+    text: post.text,
+    date: post.date,
+    id: post.id,
+  }
 }
 
 export default Post
